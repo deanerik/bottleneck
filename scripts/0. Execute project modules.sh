09c67a38d8522e3a872ba16f71d39f2243f65a11
Erik Dean <Erik.Dean@utoronto.ca>
2023-10-31 15:45:16 -0400

    added defensive programming to driver script #0

--- scripts/0. Execute project modules.sh
+++ scripts/0. Execute project modules.sh
@@ -6,41 +6,13 @@
 # or a only subset of them to either 
 # 1. download and process climate projection data
 # 2. model young-of-year overwinter survival and output results
- 
-# Needs defensive programming
 
-# print overview statement for user 
-printf "
-        ___       __  __  __                 __
-       / _ )___  / /_/ /_/ /__ ___  ___ ____/ /__
-      / _  / _ \/ __/ __/ / -_) _ \/ -_) __/  '_/
-     /____/\___/\__/\__/_/\__/_//_/\__/\__/_/\_\\
-   
-  Modelling overwinter survival under climate change 
-for young-of-year Bighead Carp in the Great Lakes basin
-  
-————————————————————————————————————————————————————————
+# ——————————————————————————————————————————————————————————————————————————————
 
-  Please give your desired output(s) and press enter.
-  There are three options:
+# define functions to be called to perform commands
 
-  1. data     -  retrieve & output climate projections
-  
-  2. results  -  run model & output simulation results
-  
-  3. both     -  performs each of the steps listed above
-
-
-
-Your selection:
-> "
-
-# read user input from keyboard
-read option
-
-# for climate data outputs, source scripts 1 to 3
-if [ $option == data ] || [ $option == both ] || [ $option = 1 ] || [ $option = 3 ] 
-then
+# for the first set of actions, run scripts 1 to 3 
+function getData {
     printf "
 
     1.         Retrieving climate data    
@@ -75,20 +47,10 @@ then
     
     # message upon completion
     printf "                    All finished!        \n\n"
-    
-# if they only want to do the second part, do nothing here & don't give an error
-elif [ $option == data ] || [ $option == both ] || [ $option = 1 ] || [ $option = 3 ] 
-then
-    :
+}
 
-# if anything else is provided
-else
-    printf "\n\n  --  Unrecognized argument given, please try again.  --  \n\n\n"
-fi
-
-# to run the model, source scripts 4-7 
-if [ $option == results ] || [ $option == both ] || [ $option = 2 ] || [ $option = 3 ] 
-then
+# for the second set of actions, run scripts 4 to 8
+function getResults {
     printf "
     2.     Running young-of-year fish model
  
@@ -128,17 +90,88 @@ then
     
     # final message upon completion
     printf "                    All finished!         \n\n"
+}
+
+# ——————————————————————————————————————————————————————————————————————————————
+
+# Needs defensive programming
+
+# Display for user running the script
+ 
+# print overview statement for user 
+printf "
+        ___       __  __  __                 __
+       / _ )___  / /_/ /_/ /__ ___  ___ ____/ /__
+      / _  / _ \/ __/ __/ / -_) _ \/ -_) __/  '_/
+     /____/\___/\__/\__/_/\__/_//_/\__/\__/_/\_\\
+   
+  Modelling overwinter survival under climate change 
+for young-of-year Bighead Carp in the Great Lakes basin
+  
+————————————————————————————————————————————————————————
+
+  Please give your desired output(s) and press enter.
+  There are three options:
+
+  1. data     -  retrieve & output climate projections
+  
+  2. results  -  run model & output simulation results
+  
+  3. both     -  performs each of the steps listed above
 
 
-# if they only wanted to do the first part, do nothing here & don't give an error
-elif [ $option == data ] || [ $option == both ] || [ $option = 1 ] || [ $option = 3 ] 
+
+Your selection:
+> "
+
+# read user input from keyboard
+read option extra
+
+# if there's more than one argument provided, return error message
+if [ -n "$extra" ]  
 then
-    :
+    printf "
+                          .-.
+                         (o.o)
+                          |=|
+                         __|__
+                       //.=|=.\\\\\\
+                      // .=|=. \\\\\\
+                      \\\\\\ .=|=. //
+                       \\\\\\(_=_)//
+                        (:| |:)
+                         || ||
+                         () ()
+                         || ||
+                         || ||
+                        ==' '==
 
-# if anything else is provided
+  --  Too many arguments given. Please use just one.  --\n\n\n"
+
+# for climate data outputs, source scripts 1 to 3
+elif [ $option == data ] || [ $option = 1 ]
+then
+    getData
+
+# to run the model, source scripts 4-8 
+elif [ $option == results ] || [ $option = 2 ]
+then
+    getResults
+
+# to do both parts if requested
+elif [ $option == both ] || [ $option = 3 ]
+then
+    getData
+    getResults
+
+# if anything else is provided as an argument, return error message
 else
-    printf "\n\n  --  Unrecognized argument given, please try again.  --  \n\n\n"
+    printf "
+                              .''
+                    ._.-.___.' (\`\\
+                   //(        ( \`'
+                  '/ )\ ).__. ) 
+                  ' <' \`\ ._/\'\\
+                     \`   \\     \\
+    \n\n  --  Unrecognized argument given, please try again.  --\n\n\n"
 fi
-
-
-
